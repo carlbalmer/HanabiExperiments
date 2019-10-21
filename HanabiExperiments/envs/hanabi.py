8740383c6aa8ed66608ac0ae341e18956bda2fe6
Carl Balmer <carl.balmer@gmail.com>
2019-10-21 16:27:03 +0200

    env.action_space.sample() now only returns legal actions

--- HanabiExperiments/envs/hanabi.py
+++ HanabiExperiments/envs/hanabi.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from random import sample
 
 from rlpyt.envs.base import Env, EnvStep
 from hanabi_learning_environment.rl_env import HanabiEnv
@@ -50,19 +51,21 @@ class WrappedHanabiEnv(Env):
         if seed:
             config["seed"] = seed
         self.env = HanabiEnv(config)
+        self._current_observation = None
 
         n_actions = 2 * hand_size + (colors + ranks) * (players - 1)
-        self._action_space = IntBox(low=0, high=n_actions, dtype="int16")
+        self._action_space = LegalActionSpace(self, low=0, high=n_actions, dtype="int16")
 
         sample_obs = numpy.array(HanabiEnv(config).reset()["player_observations"][0]["vectorized"])
         self._observation_space = IntBox(low=0, high=2, shape=sample_obs.shape, dtype=sample_obs.dtype)
 
     def step(self, action):
-        all_obs, reward, done, _ = self.env.step(action.item())
-        return EnvStep(self.extract_current_player_obs(all_obs), reward, done, None)
+        self._current_observation, reward, done, _ = self.env.step(action.item())
+        return EnvStep(self.extract_current_player_obs(self._current_observation), reward, done, None)
 
     def reset(self):
-        return self.extract_current_player_obs(self.env.reset())
+        self._current_observation = self.env.reset()
+        return self.extract_current_player_obs(self._current_observation)
 
     def legal_moves_as_int_to_bool(self, legal_moves_as_int):
         return numpy.in1d(numpy.arange(self.action_space.n), numpy.array(legal_moves_as_int))
@@ -77,6 +80,10 @@ class WrappedHanabiEnv(Env):
     def horizon(self):
         pass
 
+    @property
+    def current_observation(self):
+        return self._current_observation
+
 
 class HanabiMixin:
 
@@ -84,3 +91,12 @@ class HanabiMixin:
         return dict(input_size=env_spaces.observation.shape[0],
                     output_size=env_spaces.action.n)
 
+
+class LegalActionSpace(IntBox):
+
+    def __init__(self, env, low, high, **kwargs):
+        super(LegalActionSpace, self).__init__(low, high, **kwargs)
+        self.env = env
+
+    def sample(self):
+        return numpy.array(sample(self.env.current_observation["player_observations"][self.env.current_observation["current_player"]]["legal_moves_as_int"], 1)[0], dtype=self.dtype)
