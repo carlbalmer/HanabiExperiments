a29ce744900b15b9c090c7d89016de8f3fdfc67a
Carl Balmer <carl.balmer@gmail.com>
2019-10-21 15:31:04 +0200

    added legal moved epsilon greedy distribution

--- HanabiExperiments/agents/hanabi.py
+++ HanabiExperiments/agents/hanabi.py
@@ -4,6 +4,7 @@ from rlpyt.agents.dqn.catdqn_agent import CatDqnAgent
 from rlpyt.agents.dqn.dqn_agent import DqnAgent, AgentInfo
 from rlpyt.utils.buffer import buffer_to
 
+from HanabiExperiments.distributions.epsilon_greedy import LegalActionEpsilonGreedy
 from HanabiExperiments.envs.hanabi import HanabiMixin
 from HanabiExperiments.models.hanabi import HanabiDuelDqnModel, HanabiCatDqnModel
 
@@ -12,6 +13,22 @@ class HanabiDuelDqnAgent(HanabiMixin, DqnAgent):
 
     def __init__(self, ModelCls=HanabiDuelDqnModel, **kwargs):
         super(HanabiDuelDqnAgent, self).__init__(ModelCls=ModelCls, **kwargs)
+
+    def initialize(self, env_spaces, **kwargs):
+        super(HanabiDuelDqnAgent, self).initialize(env_spaces, **kwargs)
+        self.distribution = LegalActionEpsilonGreedy(dim=env_spaces.action.n)
+
+    @torch.no_grad()
+    def step(self, observation, prev_action, prev_reward):
+        prev_action = self.distribution.to_onehot(prev_action)
+        model_inputs = buffer_to((observation, prev_action, prev_reward),
+                                 device=self.device)
+        q = self.model(*model_inputs)
+        q = q.cpu()
+        action = self.distribution.sample(q, observation.legal_actions)
+        agent_info = AgentInfo(q=q)
+        action, agent_info = buffer_to((action, agent_info), device="cpu")
+        return AgentStep(action=action, agent_info=agent_info)
 
 
 class HanabiCatDqnAgent(HanabiMixin, CatDqnAgent):
---
+++ HanabiExperiments/distributions/epsilon_greedy.py
@@ -0,0 +1,20 @@
+import numpy
+import torch
+from rlpyt.distributions.epsilon_greedy import EpsilonGreedy
+from rlpyt.utils.tensor import infer_leading_dims, restore_leading_dims
+
+
+class LegalActionEpsilonGreedy(EpsilonGreedy):
+
+    def sample(self, q, legal_actions):
+        # add batch dimension if it is not already there
+        lead_dim, T, B, obs_shape = infer_leading_dims(q, 1)
+        q_view = q.view(T * B, * obs_shape)
+        # select action for each set of q values
+        epsion_mask = torch.rand(q_view.shape[0]) < self._epsilon
+        rand_q = torch.rand(q_view.shape)
+        rand_q[~epsion_mask] = q_view
+        rand_q[~legal_actions.view(T * B, *legal_actions.shape[-1:])] = -float('inf')
+        arg_select = torch.argmax(rand_q, dim=-1)
+        # remove batch dimension if it was not there in the beginning
+        return restore_leading_dims(arg_select, lead_dim, T, B)
