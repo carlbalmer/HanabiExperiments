4cef21f0e6f2f7a1755485c5aab9ad3ae241647f
Carl Balmer <carl.balmer@gmail.com>
2019-10-31 20:19:38 +0100

    Added proper shared reward to the env

--- rayExperiments/environment.py
+++ rayExperiments/environment.py
@@ -9,44 +9,44 @@ class MultiAgentHanabiEnv(MultiAgentEnv):
     def __init__(self, env_config):
         self.env = HanabiEnv(env_config)
         self.state = self.env.reset()
+        self.cum_reward = numpy.zeros((env_config["players"]), dtype="int")
 
-        n_actions = 2 * env_config["hand_size"] + (env_config["colors"] + env_config["ranks"]) * (env_config["players"] - 1)
+        n_actions = 2 * env_config["hand_size"] + (env_config["colors"] + env_config["ranks"]) * (
+                env_config["players"] - 1)
         self.action_space = LegalActionDiscrete(n_actions, self)
 
         sample_obs = numpy.array(self.state["player_observations"][0]["vectorized"])
-        self.observation_space = Box(low=sample_obs.min(), high=sample_obs.max(), shape=sample_obs.shape, dtype=sample_obs.dtype)
+        self.observation_space = Box(low=sample_obs.min(), high=sample_obs.max(), shape=sample_obs.shape,
+                                     dtype=sample_obs.dtype)
 
     def reset(self):
-        self.state = self.env.reset()
+        self.state = self.env.reset()  #
+        self.cum_reward[:] = 0
         current_player, current_player_obs = self.extract_current_player_obs(self.state)
         return {current_player: current_player_obs}
+
+    def step(self, action_dict):
+        current_player, _ = self.extract_current_player_obs(self.state)
+        current_player_action = action_dict[current_player]
+        self.cum_reward[current_player] = 0
+        # assert self.action_space.contains(current_player_action)
+
+        self.state, reward, done, _ = self.env.step(self.action_space.sample())  # self.env.step(current_player_action)
+        self.cum_reward + reward
+        next_player, next_player_obs = self.extract_current_player_obs(self.state)
+
+        obs_dict = {next_player: next_player_obs}
+        reward_dict = {next_player: self.cum_reward[next_player]}
+        done_dict = {"__all__": done}
+        if done:
+            obs_dict = {player: next_player_obs for player in range(len(self.cum_reward))}
+            reward_dict = {player: reward for player, reward in enumerate(self.cum_reward)}
+        return obs_dict, reward_dict, done_dict, {}
 
     def extract_current_player_obs(self, all_obs):
         current_player = all_obs["current_player"]
         current_player_obs = numpy.array(all_obs["player_observations"][current_player]["vectorized"])
         return current_player, current_player_obs
-
-    def step(self, action_dict):
-        current_player, current_player_obs = self.extract_current_player_obs(self.state)
-        current_player_action = action_dict[current_player]
-        #assert self.action_space.contains(current_player_action)
-
-        self.state, reward, done, _ = self.env.step(self.action_space.sample())  # self.env.step(current_player_action)
-        next_player, next_player_obs = self.extract_current_player_obs(self.state)
-
-        obs_dict = {next_player: next_player_obs}
-        reward_dict = {next_player: reward}
-        done_dict = {"__all__": done}
-        if done:
-            obs_dict = {0: next_player_obs,
-                        1: next_player_obs,
-                        2: next_player_obs,
-                        3: next_player_obs}
-            reward_dict = {0: reward,
-                        1: reward,
-                        2: reward,
-                        3: reward}
-        return obs_dict, reward_dict, done_dict, {}
 
 
 class LegalActionDiscrete(Discrete):
@@ -62,7 +62,8 @@ class LegalActionDiscrete(Discrete):
     def contains(self, x):
         if isinstance(x, int):
             as_int = x
-        elif isinstance(x, (numpy.generic, numpy.ndarray)) and (x.dtype.kind in numpy.typecodes['AllInteger'] and x.shape == ()):
+        elif isinstance(x, (numpy.generic, numpy.ndarray)) and (
+                x.dtype.kind in numpy.typecodes['AllInteger'] and x.shape == ()):
             as_int = int(x)
         else:
             return False
@@ -77,6 +78,6 @@ HANABI_CONF_FULL_4p = {
     "max_information_tokens": 8,
     "max_life_tokens": 3,
     "observation_type": 1,
-  #  "random_start_player": None,
-   # "seed": None
+    #  "random_start_player": None,
+    # "seed": None
 }
