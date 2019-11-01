69592e7f2733abd23cef7f1b6dba969032675df3
Carl Balmer <carl.balmer@gmail.com>
2019-11-01 14:36:48 +0100

    added resource conf to run & conda env.yml for ray

--- rayExperiments/run.py
+++ rayExperiments/run.py
@@ -5,5 +5,9 @@ from rayExperiments.environment import MultiAgentHanabiEnv, HANABI_CONF_FULL_4p
 
 tune.run(DQNTrainer, config={
     "env": MultiAgentHanabiEnv,
-    "env_config": HANABI_CONF_FULL_4p
+    "env_config": HANABI_CONF_FULL_4p,
+    "num_workers": 3,
+    "num_gpus": 1,
+    "num_gpus_per_worker": 0,
+    "num_envs_per_worker": 20
 })
