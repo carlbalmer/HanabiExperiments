02caef270f98f88b7b9191af87688e1ad7cc173b
Carl Balmer <carl@brogrammers.ch>
2020-01-13 13:03:53 +0100

    rmoved resume arg from dqn_deepmnind_baseline

--- experiments/dqn_deepmind_baseline/hanabi_dqn_deepmind.sh
+++ experiments/dqn_deepmind_baseline/hanabi_dqn_deepmind.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ray exec --start --stop --tmux experiments/dqn_deepmind_baseline/hanabi_dqn_deepmind_cluster.yaml 'python run.py -f experiments/dqn_deepmind_baseline/hanabi_dqn_deepmind.yaml --ray-address auto --resume REMOTE'
+ray exec --start --stop --tmux experiments/dqn_deepmind_baseline/hanabi_dqn_deepmind_cluster.yaml 'python run.py -f experiments/dqn_deepmind_baseline/hanabi_dqn_deepmind.yaml --ray-address auto'
 ray exec experiments/dqn_deepmind_baseline/hanabi_dqn_deepmind_cluster.yaml 'tensorboard --logdir ~/ray_results/ --port 6006' --port-forward 6006 --port-forward 8081
 # ray attach experiments/dqn_deepmind_baseline/hanabi_dqn_deephmind_cluster.yaml --tmux
