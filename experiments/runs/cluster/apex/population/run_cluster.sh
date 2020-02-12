f8133b161023b4b5c75536d130c0cbe35164a56b
Carl Balmer <carl.balmer@gmail.com>
2020-02-12 21:23:27 +0100

    restructured config

--- experiments/runs/cluster/apex/population/run_cluster.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ray exec --start --stop --tmux experiments/runs/cluster/apex/population/cluster.yaml 'python run.py -f experiments/runs/cluster/apex/population/config.yaml --ray-address auto'
-ray exec experiments/runs/cluster/apex/population/cluster.yaml 'tensorboard --logdir ~/ray_results/ --port 6006' --port-forward 6006 --port-forward 8081
-# ray attach experiments/runs/cluster/apex/population/cluster.yaml --tmux
