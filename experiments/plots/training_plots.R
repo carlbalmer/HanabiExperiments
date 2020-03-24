391c7253e3377ac982c0f89e7f9ce392d61f6c95
Carl Balmer <carl.balmer@gmail.com>
2020-03-24 16:16:41 +0100

    added apex learning and time plots

--- experiments/plots/training_plots.R
+++ experiments/plots/training_plots.R
@@ -24,6 +24,8 @@ policy_fixed_5 = read_csv("experiments/runs/dqn/local-dqn-auxtask-policy-inferen
 policy_fixed_20 = read_csv("experiments/runs/dqn/local-dqn-auxtask-policy-inference-fixed-ratio/20/progress.csv") %>% add_column(name =  "20%")
 policy_fixed_60 = read_csv("experiments/runs/dqn/local-dqn-auxtask-policy-inference-fixed-ratio/60/progress.csv") %>% add_column(name =  "60%")
 
+apex = read_csv("experiments/runs/apex/cluster-apex-default-nstep-1-larger-epsilon/LegalActionApex_Hanabi_2c5d4c4c_2020-03-13_21-04-05lyhfb2m_/progress.csv",col_names = T, cols) %>% add_column(name =  "Ape-X DQN")
+
 base_plot = ggplot() + lims(y = c(0,25)) + labs(x = "timesteps (million)", y = "game score") +theme_linedraw() + theme(aspect.ratio=1, legend.position = c(0.15,0.85), legend.title = element_blank())
 
 baseline_plot = base_plot +
@@ -64,4 +66,15 @@ hand_inference_loss_plot = base_plot +
   stat_smooth(data = bind_rows(hand_adaptive, hand_1, hand_5, hand_20, hand_60),  mapping = aes(x = timesteps_total / 1e6, y = `info/learner/default_policy/hand_inference_loss`, color = name), method = "loess", span = 0.05, n = 1000, se = F) +
   lims(x = c(0,30), y = c(0,4)) + labs(y = "auxiliary loss (cross entropy)")
 ggsave("hand_inference_loss.pdf", plot = hand_inference_loss_plot, width = 5, height = 5)
+
+apex_plot = base_plot +
+  stat_smooth(data = apex, mapping = aes(x = timesteps_total / 1e6, y = episode_reward_mean, color = name), method = "loess", span = 0.05, n = 1000) +
+  stat_smooth(data = baseline, mapping = aes(x = timesteps_total / 1e6, y = episode_reward_mean), color = "black", method = "loess", span = 0.05, n = 1000)
+ggsave("apex.pdf", plot = apex_plot, width = 5, height = 5)
+
+apex_time_plot = base_plot +
+  stat_smooth(data = apex, mapping = aes(x = time_total_s / 3600 /24 , y = episode_reward_mean, color = name), method = "loess", span = 0.05, n = 1000) +
+  stat_smooth(data = baseline, mapping = aes(x = time_total_s / 3600 / 24, y = episode_reward_mean), color = "black", method = "loess", span = 0.05, n = 1000) +
+  labs(x = "training time (days)")
+ggsave("apex_time.pdf", plot = apex_time_plot, width = 5, height = 5)
 
